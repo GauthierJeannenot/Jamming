156ecb871ce5fc03748edac4a73d3443476df3a0
Jeannenot Gauthier <jeannenot.gauthier@gmail.com>
2023-04-17 16:44:47 +0200

    changer le nom de la PlayList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,7 @@ function App() {
 
   const [searchResults, setSearchResults] = React.useState([{name:'Suck my Kiss', artist:'RedHotChilliPeppers', album:'BSSM', id:1},{name:'SockRock', artist:'ACDC', album:'Hell', id:2},{name:'Killin in the name', artist:'RATM', album:'Evil Empire', id:3}])
   const [playList, setPlayList] = React.useState([])
+  const [playListName, setPlayListName] = React.useState()
 
   
 
@@ -20,7 +21,7 @@ function App() {
       <SearchButton />
       <div id='results'>
         <SearchResults searchResults={searchResults} setPlayList={setPlayList} />
-        <PlayList playList={playList} setPlayList={setPlayList}/>
+        <PlayList playList={playList} setPlayList={setPlayList} setPlayListName={setPlayListName}/>
       </div>
 
     </>
--- src/components/PlayList/PlayList.js
+++ src/components/PlayList/PlayList.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { SaveToSpotifyButton } from '../SaveToSpotifyButton/SaveToSpotifyButton'
 import { Track } from '../Track/Track'
 
-export function PlayList({playList, setPlayList}) {
-
+export function PlayList({playList, setPlayList, setPlayListName}) {
+    const nameChange = (e) => {
+        setPlayListName(e.target.value)
+    }
 
     return(
         <div>
+            <input type='text' defaultValue='New PlayList' onChange={nameChange}></input>
             <SaveToSpotifyButton />
             {playList.map(track => <Track track={track} isRemoval={true} setPlayList={setPlayList}/>)}
         </div>
